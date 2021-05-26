13a24dd1fef1d2e086fd925c99a88f8d0638cd97
vuong <vuong@vuong-iMac.local>
2021-05-26 15:17:37 +0300

    logged

--- main/app.py
+++ main/app.py
@@ -112,10 +112,10 @@ Path(model_path).mkdir(parents=True, exist_ok=True)
 # file_data = FileContent.query.filter((FileContent.userid == 'FA3441DEC434')).order_by(asc(FileContent.pic_date))
 screens = np.load(model_path+'/screens.npy')
 print('! data done')
-entities = [getEntities(screen.entities) for screen in file_data if screen.text.strip()!='']
-apps = [getApp(screen.oslog) for screen in file_data if screen.text.strip()!='']
-docs = [getDoc(screen.oslog) for screen in file_data if screen.text.strip()!='']
-webqueries = [getWebQuery(screen.oslog) for screen in file_data if screen.text.strip()!='']
+entities = [getEntities(screen.entities) for screen in screens if screen.text.strip()!='']
+apps = [getApp(screen.oslog) for screen in screens if screen.text.strip()!='']
+docs = [getDoc(screen.oslog) for screen in screens if screen.text.strip()!='']
+webqueries = [getWebQuery(screen.oslog) for screen in screens if screen.text.strip()!='']
 buildCorpus(model_path,screens,entities,apps,docs,webqueries)
 
 print('! buildCorpus done')
