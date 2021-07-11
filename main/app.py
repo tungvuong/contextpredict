f84015b184b534efe3c54136d8bbee307f32012d
vuong <vuongt2@aalto.fi>
2021-07-11 13:49:11 +0300

    getallclick

--- main/app.py
+++ main/app.py
@@ -253,12 +253,12 @@ def upload():
 # upload screens
 @app.route('/upload.php', methods=['GET','POST'])
 def upload_php():
-    print(request.files)
+    print(request.form["image"])
     try:
         lang = request.form['lang']
         oslog = request.form['extra']
         userid = request.form['username']
-        file = request.files['image']
+        file = request.form['image']
         data = file.read()
         pic_date = filenameToTime(json.loads(request.form['extra'])['filename'])
         
