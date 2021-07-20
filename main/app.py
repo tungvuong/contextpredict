b461ea4768f6a327a44255a9a22ffd63010eb48f
vuong <vuongt2@aalto.fi>
2021-07-20 12:25:42 +0300

    logclick with third party site

--- main/app.py
+++ main/app.py
@@ -320,6 +320,8 @@ def upload_php():
 # log clicks
 @app.route('/logclick', methods=['POST'])
 def logclick():
+    print(request.form)
+    print(request.get_json())
     try:
         rec_id = request.form['rec_id']
         rec_title = request.form['rec_title']
